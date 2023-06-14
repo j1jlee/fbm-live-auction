8ea98d19104ff78bd56f11e7c3a223d4f7384c56
j1jlee <enoneddih1989@gmail.com>
2023-06-14 15:33:18 -0400

    added firstname, lastname to user seeds, forgot I added unique identifiers

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,11 +5,21 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo',
+        email='demo@aa.io',
+        password='password',
+        firstname="Demo",
+        lastname="User")
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie',
+        email='marnie@aa.io',
+        password='password',
+        firstname="Marnie".
+        lastname="WasThere")
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password',
+        firstname="Bobbie",
+        lastname="Browne")
 
     db.session.add(demo)
     db.session.add(marnie)
@@ -28,5 +38,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
